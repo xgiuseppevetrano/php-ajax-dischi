5648807a7aef0c4de6083588f0c93f85f4124d5d
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-25 02:05:37 +0200

    add milestone 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 const app = new Vue({
     el: '#app',
-    
+    data: {
+        albums: [],
+    },
+    created() {
+        axios.get('http://localhost/php-ajax-dischi/server/api.php')
+        .then((response) => {
+            this.albums = response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    },
 });
--- server/api.php
+++ server/api.php
@@ -2,4 +2,6 @@
     require __DIR__ . '/database.php';
 
     header('Content-Type: application/json');
+
+    echo json_encode($database);
 ?>
